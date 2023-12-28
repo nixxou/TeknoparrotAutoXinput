ed941eac5987ebf6276d8508f08626a355d52faa
nixx <nixx@lebrazi.com>
2023-12-28 01:18:58 +0100

    few more games

--- TeknoparrotAutoXinput/Program.cs
+++ TeknoparrotAutoXinput/Program.cs
@@ -110,7 +110,7 @@ namespace TeknoparrotAutoXinput
 
 
 				Dictionary<string, string> existingConfig = new Dictionary<string, string>();
-
+				MessageBox.Show("debug");
 				if (args.Length > 0)
 				{
 
